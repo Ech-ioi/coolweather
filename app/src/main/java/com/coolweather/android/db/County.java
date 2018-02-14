6837e5edc41614023de550bd64b967da725cf362
Ech-ioi <921692621@qq.com>
2018-02-15 00:18:38 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -6,7 +6,7 @@ import org.litepal.crud.DataSupport;
  * Created by Echo on 2018/2/13.
  */
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private int id;
     private String countyName;
     private String weatherId;
